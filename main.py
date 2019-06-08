8bfbad242010d025bdfe9ab5c5f60257df46f911
oleh <olnavr7@gmail.com>
2019-06-08 21:27:20 +0200

    fixed route lenght calculation

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ def main(args):
     r_list = [[12, 11, 5, 3], [17, 7, 5, 3]]
     anim = False
     random_seed = 1
-    for i in range(50):
-        w = Wourld(r_list, random_seed)
+    for i in range(100):
+        w = Wourld(r_list, 42)
         w.combine2Rooms()
         w.calcTargets()
+        print(w.agent_sp, w.targets)
         agent = Agent(w.agent_sp, w.targets)
         if not agent.backtracking_algorithm(w):
             print("NIE UDAŁO SIĘ ZNALEŹĆ ŚCIEŻKI")
@@ -23,7 +24,7 @@ def main(args):
             else:
                 track = agent.track[0] + agent.track[1]
                 # iteration, Shortest track,robot track
-                print(i, w.shortest_route, len(track))
+                print(random_seed, w.shortest_route-1, len(track)-1)
                 if anim:
                     anime = Anime(w, agent)
                     input()
